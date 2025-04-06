5a233098bbd85ec928888607441ae0f7dc67c9a5
hoangson2004 <lehoangson022004@gmail.com>
2025-04-07 03:23:16 +0700

    1 stream

--- src/main/java/org/aps/export_data_v2/ExportExcelService.java
+++ src/main/java/org/aps/export_data_v2/ExportExcelService.java
@@ -19,19 +19,10 @@ public class ExportExcelService {
     public byte[] exportUserToExcelSingleThreaded() throws IOException {
         SXSSFWorkbook workbook = new SXSSFWorkbook(900000);
         ExportExcelUtil.createHeaderRow(workbook);
-
-        int batchSize = 10000;
-        long totalRecords = salaryRepository.count();
-        int totalBatches = (int) Math.ceil((double) totalRecords / batchSize);
-
-        for (int batchIndex = 0; batchIndex < totalBatches; batchIndex++) {
-            Pageable pageable = PageRequest.of(batchIndex, batchSize);
-            List<Salary> batch = salaryRepository.findAll(pageable).getContent();
-            ExportExcelUtil.writeUserDataBatch(workbook, batch, batchIndex * batchSize + 2);
-        }
-
+        Pageable pageable = PageRequest.of(0, 800000);
+        List<Salary> allSalaries = salaryRepository.findAll(pageable).getContent();
+        ExportExcelUtil.writeUserDataBatch(workbook, allSalaries, 2);
         ExportExcelUtil.autoSizeColumns(workbook);
-
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         workbook.write(outputStream);
         workbook.close();
@@ -39,4 +30,5 @@ public class ExportExcelService {
 
         return outputStream.toByteArray();
     }
+
 }
